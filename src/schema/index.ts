4ee80e2f3d1908b0edf7ff9cb5da5ffaeef381e5
SamGu-NRX <sgu07966@gmail.com>
2024-07-31 10:13:07 -0500

    initialize questiononboarding

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -61,3 +61,14 @@ export const NewPasswordSchema = z.object({
       message: 'Password cannot contain spaces'
     })
 })
+
+export const FormDataSchema = z.object({
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  email: z.string().min(1, 'Email is required').email('Invalid email address'),
+  country: z.string().min(1, 'Country is required'),
+  street: z.string().min(1, 'Street is required'),
+  city: z.string().min(1, 'City is required'),
+  state: z.string().min(1, 'State is required'),
+  zip: z.string().min(1, 'Zip is required')
+})
